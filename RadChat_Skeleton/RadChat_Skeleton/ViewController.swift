0c25d863b10ed333b40d80d9f54202e7d473d14b
ZacharyWeiner <zach@cloud.com>
2014-12-22 12:38:27 -0500

    Init commit

---
+++ RadChat_Skeleton/RadChat_Skeleton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RadChat_Skeleton
+//
+//  Created by Zachary Weiner on 12/22/14.
+//  Copyright (c) 2014 zachary.weiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
